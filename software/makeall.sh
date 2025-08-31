84130ecf144aa4689ce439631af572e0b45ef981
David Court <dave@teamcourt.com>
2025-08-31 04:04:53 +0100

    Build cloudseed dependencies in makeall script, segregate binaries according to LGPL status

--- software/makeall.sh
+++ software/makeall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-for i in Mars Earth Mercury Venus Pluto Saturn Uranus
+mkdir -p built/lgpl
+
+for i in Mars Venus Pluto Saturn Uranus
 do 
     cd $i
     make -j8
@@ -12,6 +14,28 @@ do
     cd ..
 done
 
+for i in Earth Mercury
+do 
+    cd $i
+    make -j8
+    if [ $? != 0 ]
+    then
+        echo BUILD FAILED FOR $i
+        exit 1
+    fi
+    cp build/*.bin ../built/lgpl/
+    cd ..
+done
+
+cd Jupiter/CloudSeed
+make -j8
+if [ $? != 0 ]
+then
+    echo BUILD FAILED FOR Jupiter:Cloudseed
+    exit 1
+fi
+cd ../..
+
 cd Jupiter/jupiter
 make -j8
 if [ $? != 0 ]
@@ -20,6 +44,15 @@ then
     exit 1
 fi
 cp build/jupiter.bin ../../built/
+cd ../..
+
+cd Neptune/CloudSeed
+make -j8
+if [ $? != 0 ]
+then
+    echo BUILD FAILED FOR Neptune:Cloudseed
+    exit 1
+fi
 cd ../..
 
 cd Neptune/neptune
